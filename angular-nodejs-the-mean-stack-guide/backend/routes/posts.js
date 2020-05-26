3e6cf14cbd21412f01a435a933ae58bdf40bd110
Antonio Matos <acxmatos@gmail.com>
2020-05-26 18:04:38 -0300

    Unfinished section 6. Last lecture was 79

--- angular-nodejs-the-mean-stack-guide/backend/routes/posts.js
+++ angular-nodejs-the-mean-stack-guide/backend/routes/posts.js
@@ -1,9 +1,32 @@
 const Post = require("../models/post");
 const express = require("express");
+const multer = require("multer");
 
 const router = express.Router();
 
-router.post("", (req, res, next) => {
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+    destionation: (req, file, callback) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error("Invalid Mime Type");
+        if (isValid) {
+            error = null;
+        }
+        callback(error, "images");
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        callback(null, name + '-' + Date.now() + '.' + ext);
+    }
+});
+
+router.post("", multer(storage).single("image"), (req, res, next) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content,
