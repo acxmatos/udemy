d74b696bd1b6b77a08fb9dac9674bea16b3055d7
Antonio Matos <acxmatos@gmail.com>
2020-06-23 12:35:53 -0300

    Finished section 8

--- microservices-with-nodejs-and-react/ticketing/auth/src/index.ts
+++ microservices-with-nodejs-and-react/ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors"; // enables async error throwing without calling next
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -24,7 +25,24 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("v1");
-  console.log("Listening on port 3000");
-});
+// Created a separated function to use async/await in old versions of Node
+// New versions allow to use await on a top level (no need to use a function)
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("v1");
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
